2dce0f86f5b84e6d74d8862337806eae725ddb9e
grammer23 <satya.appkey@gmail.com>
2020-08-18 20:02:21 +0800

    :sparkles: Add navbar

---
+++ src/components/master/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="header">
+      <vs-navbar class="nabarx" style="box-shadow: 0 0 0 0">
+        <div slot="title">
+          <vs-navbar-title :style="{color: '#e64a19'}">
+            {{ title }}
+          </vs-navbar-title>
+        </div>
+        <vs-navbar-item index="0">
+          <g-link to="/" class="header__nav-link">Home</g-link>
+        </vs-navbar-item>
+        <vs-navbar-item index="1">
+          <g-link to="/about" class="header__nav-link">About</g-link>
+        </vs-navbar-item>
+      </vs-navbar>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+
+  &__nav-link {
+    color: #e64a19;
+  }
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="layout">
-    <!-- <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header> -->
-    <header class="header">
-      <vs-navbar v-model="activeItem" type="gradient" class="nabarx" style="box-shadow: 0 0 0 0">
-        <div slot="title">
-          <vs-navbar-title>
-            {{ $static.metadata.siteName }}
-          </vs-navbar-title>
-        </div>
-
-        <vs-navbar-item index="0">
-          <g-link to="/">Home</g-link>
-        </vs-navbar-item>
-        <vs-navbar-item index="1">
-          <g-link to="/about">About</g-link>
-        </vs-navbar-item>
-      </vs-navbar>
-    </header>
+    <Navbar :title="$static.metadata.siteName"/>
     <slot/>
   </div>
 </template>
@@ -38,39 +14,27 @@ query {
 </static-query>
 
 <script>
+import Navbar from "@/components/master/Navbar.vue"
 export default {
-  data(){
-    return {
-      activeItem: 1
-    }
+  components: {
+    Navbar
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  font-family: 'Montserrat', sans-serif;
   margin:0;
   padding:0;
   line-height: 1.5;
 }
 
 .layout {
-  max-width: 760px;
+  max-width: 920px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
 }
 </style>
