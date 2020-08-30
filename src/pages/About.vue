4881e1f242cb7a6f7e2104a1f2b5eb6290fa7ae5
satyawikananda <satyawikananda456@gmail.com>
2020-08-30 23:53:52 +0800

    :zap: Improve about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,40 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <vs-row vs-w="12" class="margin-v-xl">
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="12" vs-sm="12" vs-xs="12">
+        <vs-card class="cardx padding" fixedHeight>
+          <center><img src="https://avatars1.githubusercontent.com/u/33148052?v=4" alt="satya-pic" class="avatar-rounded"/></center>
+          <vs-divider color="#E64A19"><h2>Tentang yang punya blog</h2></vs-divider>
+          <p style="font-size: .9rem" class="text-center">Hallo 👋. Nama ku <b style="color: #E64A19">I Gusti Ngurah Satya Wikananda</b>, biasa dipanggil <b style="color: #E64A19">Satya</b>, aku sekarang berumur {{ getAge("2001/04/23") }}. Tujuanku menulis adalah hanya ingin membagikan kepada pembaca apa yang sudah aku pelajari dan apa yang sudah aku dapatkan. Untuk tema blog aku ini, mungkin bisa seputar pengembangan software seperti website dan teknologi lainnya juga, dan aku juga bakal share tentang diluar topik yang sudah aku sebutkan tadi. Oh ya, kalau kalian ada sesuatu yang bisa kalian bagikan, kalian bisa bikin <i>pull request</i> di repo blog ini di github. Semoga bermanfaat, sekian dan terimakasih 😎</p>
+          <vs-row vs-w="12" class="margin-v">
+            <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="12" vs-sm="12" vs-xs="12">
+              <vs-button href="https://github.com/satyawikananda/grammer-blog" size="medium" color="#FF4E50" gradient-color-secondary="#F9D423" type="gradient" icon="share">Ayo kontribusi</vs-button>
+            </vs-col>
+          </vs-row>
+        </vs-card>
+      </vs-col>
+    </vs-row>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
+    title: 'About us',
+  },
+  methods: {
+    getAge(dateString) {
+      const today = new Date()
+      const birthDate = new Date(dateString)
+      let age = today.getFullYear() - birthDate.getFullYear()
+      const month = today.getMonth() - birthDate.getMonth()
+
+      if(month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        age--
+      }
+
+      return age
+    }
   }
 }
 </script>
